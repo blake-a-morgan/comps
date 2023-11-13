e83a5cf54ea6a0657f01086cc9a7b7c206cb9c9e
Blake Morgan <blakemorgan@gmail.com>
2023-11-13 01:42:14 -0600

    Added Route, Link, and Sidebar components.Sidebar with links show. Pages display when links are pressed. Created hook for navigation.

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,7 +8,7 @@ function Button({
     rounded,
     ...rest 
 }){
-    console.log(rest);
+    
 
     const classes = twMerge(classnames(rest.className, ' flex items-center px-3 py-1.5 border',{
         'border-blue-500 bg-blue-500 text-white': purpose === 'primary',
