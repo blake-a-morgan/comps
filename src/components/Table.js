f43c3a225cf22bac662b0089aa2b06e7e0fc068a
Blake Morgan <blakemorgan@gmail.com>
2023-11-30 01:59:23 -0600

    Sortable Table component added. TablePage now showing Sortable table. Sort States added to ST Component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,11 @@
+import {Fragment} from 'react';
+
 function Table({data, config, keyFn}){
 
     const renderedHeaders = config.map((column) => {
+        if(column.header){
+            return <Fragment key={column.label}>{column.header()}</Fragment>
+        }
         return <th key={column.label}>{column.label}</th>
     });
 
@@ -25,6 +30,6 @@ function Table({data, config, keyFn}){
             {renderedRows}
         </tbody>
     </table>
-}
+};
 
 export default Table;
